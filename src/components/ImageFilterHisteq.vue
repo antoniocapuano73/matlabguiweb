05c0960a7da6f2e06ad4d92f556a441c1be5f1f5
antoniocapuano73 <antoniocapuano@hotmail.com>
2020-06-23 20:37:05 +0200

    200623_AC_02

---
+++ src/components/ImageFilterHisteq.vue
@@ -0,0 +1,111 @@
+<template>
+  <CContainer>
+    <CRow>
+      <CCol Cols="6">
+        <OpenImageDialog
+          :textButton="textButtonSelectImage"
+          :onClick="onClickSourceImage"
+          :onLoad ="onLoadSourceImage">
+        </OpenImageDialog>
+      </CCol>
+      <CCol Cols="6">
+        <ShowImageDialog
+          :textButton="textButtonFilterImage"
+          :imageDataModel="imageDataModelDestination"
+          :onClick="onUpload"
+          :onLoad ="onLoadDestinationImage">
+        </ShowImageDialog>
+      </CCol>
+    </CRow>
+  </CContainer>
+</template>
+
+<script>
+
+// import components
+import OpenImageDialog from '@/components/OpenImageDialog.vue';
+import ShowImageDialog from '@/components/ShowImageDialog.vue';
+
+// import code
+import {ImageDataModel} from '@/api/models/ApiGlobalModels.js';
+import {loadImage}      from '@/lib/images/TagImages.js';
+import {apply0P}        from '@/api/images/ImageFilterController.js';
+
+import {
+  ctxImage,
+  getImageData,
+  setImageData
+} from '@/lib/images/TagImages.js'
+
+export default {
+  name: 'ImageFilterHisteq',
+  components: {
+    OpenImageDialog, 
+    ShowImageDialog,
+  },
+  data: function () {
+    return {
+      imageDataModelSource: null,
+      imageDataModelDestination: null
+    }
+  },
+  props: {
+    textButtonSelectImage: {
+      type: String,
+      default: 'Select image'
+    },
+    textButtonFilterImage: {
+      type: String,
+      default: 'Filter image'
+    }
+  },
+  methods: {
+    onClickSourceImage: function() {
+
+    },
+    onLoadSourceImage: function(imageDataModel) {
+      // return
+      this.imageDataModelSource = imageDataModel;
+
+      // clear destination image
+      this.imageDataModelDestination = null;
+    },
+    onUpload: function() {
+      let that = this;
+
+      // image destination
+      let tagImageDestination = document.getElementById('imageDestination');
+      that.imageDataModelDestination = null;
+
+      // debug
+      // console.log(that.imageDataModelSource);
+
+      // filter
+      if (that.imageDataModelSource !== null) {
+        apply0P('histeq',that.imageDataModelSource, 
+          function (imageDataModel) {
+
+            //console.log(imageDataModel);
+
+            that.imageDataModelDestination = imageDataModel;
+            setImageData(tagImageDestination,imageDataModel);
+
+        });
+
+      }
+    },
+    onLoadDestinationImage: function(imageDataModel) {
+    
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+  .preview-image {
+    border-style: solid; 
+    border-width: 1px;
+    width: 100%;
+  }
+</style>
--- src/views/imagefilters/Histeq.vue
+++ src/views/imagefilters/Histeq.vue
@@ -1,101 +1,29 @@
 <template>
   <CContainer>
-    <CRow>
-      <CCol Cols="6">
-        <OpenImageDialog
-          textButton='Select image'
-          :onClick="onClickSourceImage"
-          :onLoad ="onLoadSourceImage">
-        </OpenImageDialog>
-      </CCol>
-      <CCol Cols="6">
-        <ShowImageDialog
-          textButton='Filter image'
-          :imageDataModel="imageDataModelDestination"
-          :onClick="onUpload"
-          :onLoad ="onLoadDestinationImage">
-        </ShowImageDialog>
-      </CCol>
-    </CRow>
+    <ImageFilterHisteq textButtonSelectImage="Select Image" textButtonFilterImage="Filter Image">
+    </ImageFilterHisteq>
   </CContainer>
 </template>
 
 <script>
 
 // import components
-import OpenImageDialog from '@/components/OpenImageDialog.vue';
-import ShowImageDialog from '@/components/ShowImageDialog.vue';
-
-// import code
-import {ImageDataModel} from '@/api/models/ApiGlobalModels.js';
-import {loadImage}      from '@/lib/images/TagImages.js';
-import {apply0P}        from '@/api/images/ImageFilterController.js';
-
-import {
-  ctxImage,
-  getImageData,
-  setImageData
-} from '@/lib/images/TagImages.js'
+import ImageFilterHisteq from '@/components/ImageFilterHisteq.vue';
 
 export default {
   name: 'Histeq',
   components: {
-    OpenImageDialog, 
-    ShowImageDialog,
+    ImageFilterHisteq
   },
   data: function () {
     return {
-      imageDataModelSource: null,
-      imageDataModelDestination: null
+
     }
   },
   methods: {
-    onClickSourceImage: function() {
 
-    },
-    onLoadSourceImage: function(imageDataModel) {
-      // return
-      this.imageDataModelSource = imageDataModel;
-
-      // clear destination image
-      this.imageDataModelDestination = null;
-    },
-    onUpload: function() {
-      let that = this;
-
-      // image destination
-      let tagImageDestination = document.getElementById('imageDestination');
-      that.imageDataModelDestination = null;
-
-      // debug
-      // console.log(that.imageDataModelSource);
-
-      // filter
-      if (that.imageDataModelSource !== null) {
-        apply0P('histeq',that.imageDataModelSource, 
-          function (imageDataModel) {
-
-            //console.log(imageDataModel);
-
-            that.imageDataModelDestination = imageDataModel;
-            setImageData(tagImageDestination,imageDataModel);
-
-        });
-
-      }
-    },
-    onLoadDestinationImage: function(imageDataModel) {
-    
-    }
   }
 }
 
 </script>
 
-<style scoped>
-  .preview-image {
-    border-style: solid; 
-    border-width: 1px;
-    width: 100%;
-  }
-</style>
